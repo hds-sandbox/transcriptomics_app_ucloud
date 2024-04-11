b7ff7d1ea09b2337b1953013644f25230fb988f4
SamueleSoraggi <samuelson1990@gmail.com>
2024-04-11 10:28:48 +0200

    enabled welcome message

--- scripts/set_Rprofile.R
+++ scripts/set_Rprofile.R
@@ -1,8 +1,9 @@
-#setHook("rstudio.sessionInit", function(newSession) {
-#  if (newSession)
-#    rstudioapi::navigateToFile('/opt/RNAseq_in_Rstudio/welcome_messaoject = ge.md', line = -1L, column = -1L)
-#}, action = "append")
+setHook("rstudio.sessionInit", function(newSession) {
+  if (newSession)
+    rstudioapi::navigateToFile('/opt/RNAseq_in_Rstudio/welcome_message.md', line = -1L, column = -1L)
+}, action = "append")
 
 
 Sys.setenv(RENV_PATHS_CACHE = '/opt/RNAseq_in_Rstudio/cache')
 try( renv::load('/opt/RNAseq_in_Rstudio') )
+message("IGNORE the error message above. It is expected due to a bug in renv.")
