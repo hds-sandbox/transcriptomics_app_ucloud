d9fbc8de077ffa783c3f8497803821a2c6fe7efd
SamueleSoraggi <samuelson1990@gmail.com>
2024-04-15 10:06:44 +0200

    changed welcome message

--- scripts/set_Rprofile.R
+++ scripts/set_Rprofile.R
@@ -6,4 +6,4 @@
 
 Sys.setenv(RENV_PATHS_CACHE = '/opt/RNAseq_in_Rstudio/cache')
 suppressWarnings(renv::load('/opt/RNAseq_in_Rstudio'))
-message("WELCOME to the Rstudio setup of the Transcriptomics Sandbox. For more information, visit our page https://hds-sandbox.github.io")
+message("----------------------\nWelcome to the Transcriptomcis setup in the Rstudio development environment.\n----------------------\nIf you found this app useful for your own research, please ACKNOWLEDGE us by adding this prompt to your research:\n\tWe acknowledge assistance and support for computational resources and infrastructure\n\tvia the Health Data Science Sandbox (https://hds-sandbox.github.io), funded by the Novo Nordisk Foundation (NNF20OC0063268).\n----------------------\nWe also welcome suggestions and bug reports. Send an email to NHDS_sandbox@sund.ku.dk.\n----------------------\nThank you very much!\nThe NHDS Sandbox team. https://hds-sandbox.github.io\n----------------------\n")
